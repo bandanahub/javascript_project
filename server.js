d38f2d5abcad86d5da683b965f38e897ca37fa90
Bandana Chaulagain <138704329+bandanahub@users.noreply.github.com>
2023-08-16 01:11:56 -0400

    Update server.js

--- server.js
+++ server.js
@@ -4,21 +4,26 @@ const mongoose = require('mongoose');
 
 const app = express();
 
+// Middleware
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-// Connect to MongoDB (replace 'your_db_url' with your actual MongoDB connection string)
-mongoose.connect('your_db_url', { useNewUrlParser: true, useUnifiedTopology: true })
+// MongoDB Connection
+const dbUrl = 'your_db_url'; // Replace with your MongoDB connection string
+mongoose.connect(dbUrl, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('Connected to MongoDB'))
     .catch(err => console.error('MongoDB connection error:', err));
 
-// Define a User model (replace with your actual user schema)
+// User Model
 const User = mongoose.model('User', {
     name: String,
     email: String,
     password: String
 });
 
+// Routes
+
+// User Registration
 app.post('/register', async (req, res) => {
     const { name, email, password } = req.body;
 
@@ -39,12 +44,7 @@ app.post('/register', async (req, res) => {
     }
 });
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Server started on port ${PORT}`);
-});
-// ... (existing code) ...
-
+// User Login
 app.post('/login', async (req, res) => {
     const { email, password } = req.body;
 
@@ -61,4 +61,34 @@ app.post('/login', async (req, res) => {
         console.error('Error during login:', error);
         res.status(500).json({ success: false });
     }
+});
+
+// Product Data
+const products = [
+    {
+        id: 1,
+        name: 'Product A',
+        imageUrl: 'product-a.jpg',
+        price: 10.99,
+        quantity: 20
+    },
+    {
+        id: 2,
+        name: 'Product B',
+        imageUrl: 'product-b.jpg',
+        price: 19.99,
+        quantity: 15
+    },
+    // Add more products...
+];
+
+// Product Listing
+app.get('/products', (req, res) => {
+    res.json({ products: products });
+});
+
+// Server Start
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server started on port ${PORT}`);
 });
