ab158ec7daecb1ad82ab8264b9e1feed92ed7934
Bandana Chaulagain <138704329+bandanahub@users.noreply.github.com>
2023-08-16 01:11:38 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 $(document).ready(function () {
+    // Registration Form
     $('#registration-form').submit(function (event) {
         event.preventDefault();
         
@@ -32,12 +33,8 @@ $(document).ready(function () {
             }
         });
     });
-});
 
-
-$(document).ready(function () {
-    
-
+    // Login Form
     $('#login-form').submit(function (event) {
         event.preventDefault();
 
@@ -62,5 +59,51 @@ $(document).ready(function () {
                 $('#login-message').html('<p>Error processing the request.</p>');
             }
         });
+    });
+
+    // Fetch and render product list
+    function renderProductList(products) {
+        const productListDiv = $('#product-list');
+        productListDiv.empty();
+
+        products.forEach(product => {
+            const productDiv = $('<div class="product"></div>');
+            productDiv.append(`<img src="${product.imageUrl}" alt="${product.name}" class="product-image">`);
+            productDiv.append(`<h3 class="product-name">${product.name}</h3>`);
+            productDiv.append(`<p class="product-price">Price: $${product.price}</p>`);
+            productDiv.append(`<p class="product-quantity">Quantity: ${product.quantity}</p>`);
+            productListDiv.append(productDiv);
+        });
+    }
+
+    // Fetch product data from the server
+    function fetchProductData(searchTerm = '') {
+        $.ajax({
+            type: 'GET',
+            url: '/products',
+            success: function (response) {
+                let filteredProducts = response.products;
+
+                if (searchTerm) {
+                    filteredProducts = response.products.filter(product => {
+                        return product.name.toLowerCase().includes(searchTerm);
+                    });
+                }
+
+                renderProductList(filteredProducts);
+            },
+            error: function () {
+                console.log('Error fetching product data.');
+            }
+        });
+    }
+
+    // Initial fetch on page load
+    fetchProductData();
+
+    // Search functionality
+    $('#search-input').on('input', function () {
+        const searchTerm = $(this).val().toLowerCase();
+        fetchProductData(searchTerm);
     });
 });
