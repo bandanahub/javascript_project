c5b4560384e8f4dcc32b3a979f3f849832cd0e24
Bandana Chaulagain <138704329+bandanahub@users.noreply.github.com>
2023-08-18 00:14:55 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,4 @@
 $(document).ready(function () {
-    $("#userSelectionForm").submit(function (event) {
-        event.preventDefault();
-
-        const userType = $("input[name='userType']:checked").val();
-
-        if (userType === "new") {
-            window.location.href = "register.html";
-        } else if (userType === "existing") {
-            window.location.href = "login.html";
-        }
-    });
-});
-
-$(document).ready(function () {
-    // Show user selection form
-    $('.user-selection').show();
-
     // User Selection Form Submission
     $('#userSelectionForm').submit(function (event) {
         event.preventDefault();
@@ -23,11 +6,9 @@ $(document).ready(function () {
         const userType = $('input[name="userType"]:checked').val();
 
         if (userType === 'new') {
-            $('.user-selection').hide();
-            $('#registration-form').show();
+            window.location.href = "register.html";
         } else if (userType === 'existing') {
-            $('.user-selection').hide();
-            $('#login-form').show();
+            window.location.href = "login.html";
         }
     });
 
@@ -35,29 +16,94 @@ $(document).ready(function () {
     $('#registration-form').submit(function (event) {
         event.preventDefault();
 
-        // ... (existing registration form logic) ...
+        const password = $('#password').val();
+        const confirmPassword = $('#confirm-password').val();
+
+        if (password !== confirmPassword) {
+            $('#message').text('Passwords do not match.');
+            return; // Prevent form submission
+        }
+
+        const formData = $(this).serialize(); // Serialize form data
+        // Simulating successful registration (replace with actual registration logic)
+        setTimeout(function () {
+            window.location.href = "products.html"; // Redirect to products.html
+        }, 1000); // Redirect after 1 second (for simulation)
     });
 
+    // Login Form
     // Login Form
     $('#login-form').submit(function (event) {
         event.preventDefault();
 
-        // ... (existing login form logic) ...
+        const email = $('#login-email').val();
+        const password = $('#login-password').val();
+
+        // Send the email and password to the server for validation
+        $.ajax({
+            type: "POST",
+            url: "/login", // Use the correct server endpoint
+            data: { email: email, password: password },
+            success: function (response) {
+                // Handle successful login response
+                window.location.href = "products.html"; // Redirect to products.html
+            },
+            error: function (error) {
+                $('#login-message').text('Invalid email or password.'); // Display error message
+            }
+        });
     });
 
     // Fetch and render product list
     function renderProductList(products) {
-        // ... (existing renderProductList function logic) ...
+        const productListContainer = $('#product-list');
+        productListContainer.empty(); // Clear previous content
+
+        if (products.length === 0) {
+            productListContainer.append('<p>No products found.</p>');
+        } else {
+            const ul = $('<ul>');
+            products.forEach(product => {
+                const li = $('<li>');
+                li.text(product.name + ' - $' + product.price);
+                ul.append(li);
+            });
+            productListContainer.append(ul);
+        }
     }
 
     // Add to Cart Button Click
     $('#add-to-cart-btn').click(function () {
-        // ... (existing add-to-cart logic) ...
+        const productId = $(this).data('product-id'); // Get the product ID
+        // Send the productId to the server using an AJAX request
+        $.ajax({
+            type: "POST",
+            url: "addToCart.php", // Replace with your server endpoint
+            data: { productId: productId },
+            success: function (response) {
+                // Handle successful add to cart response
+            },
+            error: function (error) {
+                // Handle add to cart error
+            }
+        });
     });
 
     // Fetch product data from the server
     function fetchProductData(searchTerm = '') {
-        // ... (existing fetchProductData function logic) ...
+        // Send searchTerm to the server using an AJAX request
+        $.ajax({
+            type: "GET",
+            url: "getProducts.php", // Replace with your server endpoint
+            data: { searchTerm: searchTerm },
+            success: function (response) {
+                const products = response.products; // Assuming server responds with product data
+                renderProductList(products);
+            },
+            error: function (error) {
+                // Handle fetch product data error
+            }
+        });
     }
 
     // Initial fetch on page load
